54d53de6c022a4ed61cfa70934a2d915b77ced6f
Alex Swiggum <alexswiggum@Alexs-MBP.localdomain>
2023-07-03 20:06:02 -0500

    updated models & api routes

--- streaming_app/models.py
+++ streaming_app/models.py
@@ -30,7 +30,7 @@ class Show(db.Model):
         self.watch_id = self.set_id()
         self.title = title
         self.type_ = type_
-        self.poster_image = poster_image
+        self.poster_image = self.set_image(poster_image)
         self.genre = genre
         self.summary = summary
         self.rating = rating
@@ -42,6 +42,9 @@ class Show(db.Model):
 
     def set_id(self):
         return str(uuid.uuid4())
+      
+    def set_image(self, img):
+        return f"https://image.tmdb.org/t/p/w500{img}"
 
     def __repr__(self):
         return f"Show {self.title} has been added to the database!"
@@ -69,30 +72,31 @@ class ShowCast(db.Model):
 
 class Cast(db.Model):
     cast_id = db.Column(db.String, primary_key = True)
-    first_name = db.Column(db.String(150))
-    last_name = db.Column(db.String(150))
+    full_name = db.Column(db.String(150))
     cast_image = db.Column(db.String(150))
     bio_link = db.Column(db.String(150))
     show_cast = db.relationship('ShowCast', backref = 'cast', lazy = True)
 
-    def __init__(self, first_name, last_name, cast_image, bio_link, cast_id = ''):
+    def __init__(self, full_name, cast_image, bio_link, cast_id = ''):
         self.cast_id = self.set_id()
-        self.first_name = first_name
-        self.last_name = last_name
-        self.cast_image = cast_image
+        self.full_name = full_name
+        self.cast_image = self.set_image(cast_image)
         self.bio_link = bio_link 
 
     def set_id(self):
         return str(uuid.uuid4())
+      
+    def set_image(self, img):
+        return f"https://image.tmdb.org/t/p/w500{img}"
 
     def __repr__(self):
-        return f"{self.first_name.title(), self.last_name.title()} has been added to the database!"
+        return f"{self.full_name.title()} has been added to the database!"
 
 
-class Hub(db.Model):
+class ShowHub(db.Model):
     watch_id = db.Column(db.String, primary_key = True)
     title = db.Column(db.String(150))
-    type = db.Column(db.String(150))
+    type_ = db.Column(db.String(150))
     poster_image = db.Column(db.String(150))
     genre = db.Column(db.String(150))
     summary = db.Column(db.String)
@@ -102,16 +106,16 @@ class Hub(db.Model):
     review_score = db.Column(db.String, nullable=True)
     review = db.Column(db.String, nullable=True)
     how_many_reviews = db.Column(db.Numeric(precision=1, scale=0), nullable=True)
+    total_review = db.Column(db.Numeric(precision=3, scale=2), nullable=True)
     date_added = db.Column(db.DateTime,default = datetime.utcnow)
-    user_id = db.Column(db.String)
     # show_cast = db.relationship('ShowCast', backref = 'hub', lazy = True)
 
 
 
-    def __init__(self, watch_id, title, type, poster_image, genre, summary, rating, streaming, review_score = None, review = '', how_many_reviews = None, have_watched = False, user_id = ''):
+    def __init__(self, watch_id, title, type_, poster_image, genre, summary, rating, streaming, review_score = None, review = '', how_many_reviews = None, total_review = None, have_watched = False):
         self.watch_id = self.set_id(watch_id)
         self.title = title
-        self.type = type
+        self.type_ = type_
         self.poster_image = poster_image
         self.genre = genre
         self.summary = summary
@@ -121,13 +125,75 @@ class Hub(db.Model):
         self.review_score = review_score
         self.review = review
         self.how_many_reviews = how_many_reviews
-        self.user_id = user_id
+        self.total_review = total_review
 
     def set_id(self,id):
         return f"HUB{id}"
+      
+#     def calculate_review(self, reviews, count):
+#         if reviews and count:
+#             print('total review score is..')
+#             print(round(sum([float(score) for score in reviews.split(',')])/ float(count), 2))
+#             return round(sum([float(score) for score in reviews.split(',')])/ float(count), 2)
+          
+#         return None
+        
 
     def __repr__(self):
         return f"Show {self.title} has been added to the HUB database!"
+      
+class UserHub(db.Model):
+    userhub_id = db.Column(db.String, primary_key = True)
+    hub_id = db.Column(db.String, db.ForeignKey('hub.hub_id'), nullable = False)
+    user_id = db.Column(db.String)
+    
+    def __init__(self, hub_id, user_id, userhub_id = ""):
+        self.userhub_id = self.set_id()
+        self.hub_id = hub_id
+        self.user_id = user_id
+        
+    def set_id(self):
+        return str(uuid.uuid4())
+
+    def __repr__(self):
+        return f"Your user relationship {self.userhub_id} has been added to the database!"
+      
+      
+class UserWatchHub(db.Model):
+    watchhub_id = db.Column(db.String, primary_key = True)
+    hub_id = db.Column(db.String, db.ForeignKey('hub.hub_id'), nullable = False)
+    watch_id = db.Column(db.String, db.ForeignKey('show_hub.watch_id'), nullable = False)
+    date_created = db.Column(db.DateTime, nullable = False, default = datetime.utcnow)
+    
+    def __init__(self, hub_id, watch_id, watchhub_id = ""):
+        self.watchhub_id = self.set_id()
+        self.hub_id = hub_id
+        self.watch_id = watch_id
+        
+        
+    def set_id(self):
+        return str(uuid.uuid4())
+
+    def __repr__(self):
+        return f"Your movie relationship {self.hub_id} has been added to the database!"
+      
+      
+class Hub(db.Model):
+    hub_id = db.Column(db.String, primary_key = True)
+    hub_name = db.Column(db.String(50))
+    created_by = db.Column(db.String)
+    
+    def __init__(self, hub_name, created_by, hub_id = ""):
+        self.hub_id = self.set_id()
+        self.hub_name = hub_name
+        self.created_by = created_by
+        
+    def set_id(self):
+        return str(uuid.uuid4())
+      
+    def __repr__(self):
+        return f"Your hub {self.hub_name} has been added to the database!"
+    
     
 
 
@@ -135,11 +201,16 @@ class Hub(db.Model):
 class ShowSchema(ma.Schema):
     class Meta:
         fields = ['watch_id', 'title', 'type_', 'poster_image', 'genre', 'summary', 'rating', 
-                  'streaming', 'have_watched', 'review_score', 'review', 'how_many_reviews', 'user_id']
+                  'streaming', 'have_watched', 'review_score', 'review', 'how_many_reviews', 'total_review', 'user_id']
         
 class CastSchema(ma.Schema):
     class Meta:
-        fields = ['cast_id', 'first_name', 'last_name', 'cast_image', 'bio_link']
+        fields = ['cast_id', 'full_name', 'cast_image', 'bio_link']
+        
+        
+class HubSchema(ma.Schema):
+    class Meta:
+        fields = ['hub_id', 'hub_name', 'created_by']
 
 
 show_schema = ShowSchema()
@@ -148,6 +219,9 @@ shows_schema = ShowSchema(many = True)
 
 cast_schema = CastSchema()
 casts_schema = CastSchema(many = True)
+
+hub_schema = HubSchema()
+hubs_schema = HubSchema(many = True)
 
 
     
